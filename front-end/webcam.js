ac1f6a2c04aae9c483f09d30908bed3496ec67bd
Matthew Jörke <matthewjoerke@gmail.com>
2021-04-29 16:07:53 -0700

    camera app working prototype

---
+++ front-end/webcam.js
@@ -0,0 +1,66 @@
+// adapted from: https://github.com/abenjamin765/camera-app/blob/master/part-1/app.js
+
+// Set constraints for the video stream
+var constraints = { video: { facingMode: "environment" }, audio: false };
+// video MediaStreamTrack object
+var track;
+
+// Define constants
+const cameraViewport = document.querySelector("#camera-viewport");
+const cameraCanvas   = document.querySelector("#camera-canvas");
+const cameraImage    = document.querySelector("#camera-image");
+const cameraButton   = document.querySelector("#camera-button");
+const downloadButton = document.querySelector("#download-button");
+const swapButton     = document.querySelector("#swap-camera-button");
+
+// Access the device camera and stream to cameraView
+function cameraStart() {
+    navigator.mediaDevices
+        .getUserMedia(constraints)
+        .then(function(stream) {
+            track = stream.getTracks()[0];
+            // webcamWidth, webcamHeight = track.getSettings()
+            cameraViewport.srcObject = stream;
+        })
+        .catch(function(error) {
+            console.error("Error reading camera stream.", error);
+        });
+}
+
+// Take a picture when cameraTrigger is tapped
+cameraButton.onclick = function() {
+    let w = cameraViewport.videoWidth;
+    let h = cameraViewport.videoHeight;
+
+    cameraImage.width  = w;
+    cameraImage.height = h;
+
+    cameraCanvas.width = w;
+    cameraCanvas.height = h;
+
+    cameraCanvas.getContext("2d").drawImage(cameraViewport, 0, 0, w, h);
+    cameraImage.src = cameraCanvas.toDataURL("image/webp");
+    cameraImage.classList.add("taken");
+
+    downloadButton.href = cameraCanvas.toDataURL("image/png").replace(/^data:image\/[^;]/, 'data:application/octet-stream');
+
+    cameraCanvas.getContext("2d").clearRect(0, 0, w, h)
+    // track.stop();
+};
+
+swapButton.onclick = function() {
+    if (constraints.video.facingMode == "environment") {
+        constraints.video.facingMode = "user";
+    } else if (constraints.video.facingMode == "user") {
+        constraints.video.facingMode = "environment";
+    }
+
+    cameraStart();
+
+    cameraCanvas.classList.toggle('flipped');
+    cameraImage.classList.toggle('flipped');
+    cameraViewport.classList.toggle('flipped');
+}
+
+// Start the video stream when the window loads
+window.addEventListener("load", cameraStart, false);
