9b53d4c84ffafcf0724b4dbea4a3c3b159314dcb
Anelise Newman <anelisenewman@gmail.com>
2021-05-11 18:54:43 -0400

    Endpoints tosave and load audio stories

--- server/index.py
+++ server/index.py
@@ -7,14 +7,18 @@ import flask
 from PIL import Image
 from werkzeug.exceptions import HTTPException
 
-from server import model_utils
+from server.model_utils import CLIPModel
+from server.save_audio_story import AudioStoryLoader
+from server.speech_generator import DummySpeechGenerator
 
 app = flask.Flask(__name__)
 model = None
+audio_story_loader = None
 
 AUDIO_FILES_ENDPOINT = "/audio-files/"
-AUDIO_FILES_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)),
-                               "server_data", "audio")
+AUDIO_STORY_FILES_DIR = os.path.join(
+    os.path.dirname(os.path.realpath(__file__)), "server_data")
+AUDIO_FILES_DIR = os.path.join(AUDIO_STORY_FILES_DIR, "audio")
 
 
 class InvalidLabelException(Exception):
@@ -78,9 +82,45 @@ def serve_audio_file(path):
     return flask.send_from_directory(AUDIO_FILES_DIR, path)
 
 
-def run_app(debug=True, port=5000, host="0.0.0.0", device="cuda"):
+@app.route('/save-audio-story', methods=['POST'])
+def save_audio_story():
+    global audio_story_loader
+
+    audio_story = flask.request.json
+    audio_story_id = audio_story["story_id"]
+    audio_story_loader.save(audio_story,
+                            audio_story_id,
+                            check_exists=True,
+                            generate_audio=True)
+
+
+@app.route('/load-audio-story/<string:story_id>')
+def load_audio_story(story_id):
+    global audio_story_loader
+
+    graph = audio_story_loader.load(story_id,
+                                    must_have_audio=True,
+                                    audio_relative_to=AUDIO_FILES_DIR)
+
+    return flask.jsonify(graph)
+
+
+def run_app(debug=True,
+            port=5000,
+            host="0.0.0.0",
+            device="cuda",
+            save_dir=AUDIO_STORY_FILES_DIR,
+            audio_save_dir=AUDIO_FILES_DIR):
+
     global model
-    model = model_utils.CLIPModel(device)
+    global audio_story_loader
+
+    model = CLIPModel(device)
+
+    speech_generator = DummySpeechGenerator()
+    audio_story_loader = AudioStoryLoader(save_dir=save_dir,
+                                          audio_save_dir=audio_save_dir,
+                                          speech_generator=speech_generator)
     app.run(debug=debug, port=port, host=host)
 
 
