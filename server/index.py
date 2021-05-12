fe001a4582ea06e6f4afb282089b5160c280a081
Anelise Newman <anelisenewman@gmail.com>
2021-05-11 21:13:05 -0400

    better factored server and automated api tests

--- server/index.py
+++ server/index.py
@@ -1,108 +1,7 @@
-import http
-import json
-import os
-
 import fire
-import flask
-from PIL import Image
-from werkzeug.exceptions import HTTPException
 
-from server.model_utils import CLIPModel
-from server.save_audio_story import AudioStoryLoader
-from server.speech_generator import DummySpeechGenerator
-
-app = flask.Flask(__name__)
-model = None
-audio_story_loader = None
-
-AUDIO_FILES_ENDPOINT = "/audio-files/"
-AUDIO_STORY_FILES_DIR = os.path.join(
-    os.path.dirname(os.path.realpath(__file__)), "server_data")
-AUDIO_FILES_DIR = os.path.join(AUDIO_STORY_FILES_DIR, "audio")
-
-
-class InvalidLabelException(Exception):
-    pass
-
-
-@app.errorhandler(HTTPException)
-def handle_exception(e):
-    """Return JSON instead of HTML"""
-    response = e.get_response()
-    response.data = json.dumps({
-        "code": e.code,
-        "name": e.name,
-        "description": e.description
-    })
-    response.content_type = "application/json"
-
-    return response
-
-
-@app.errorhandler(InvalidLabelException)
-def handle_invalid_label_exception(e):
-    return str(e), http.HTTPStatus.UNPROCESSABLE_ENTITY
-
-
-@app.route('/')
-def index():
-    return "This is the audio stories server."
-
-
-@app.route('/check')
-def check():
-    return "It's working!"
-
-
-@app.route('/inference', methods=['POST'])
-def inference():
-    global model
-
-    img_file = flask.request.files['image']
-    image = Image.open(img_file.stream)
-
-    labels_json = flask.request.form['labels']
-    try:
-        labels = json.loads(labels_json)
-    except ValueError as ve:
-        raise InvalidLabelException(
-            "Invalid labels provided: {}. Labels must be JSON. Produced the following error: {}"
-            .format(labels_json, str(ve)))
-
-    scores = model.inference(image, labels).tolist()
-
-    response = flask.jsonify(scores)
-    response.headers.add('Access-Control-Allow-Origin', '*')
-
-    return response
-
-
-@app.route('{}<path:path>'.format(AUDIO_FILES_ENDPOINT))
-def serve_audio_file(path):
-    return flask.send_from_directory(AUDIO_FILES_DIR, path)
-
-
-@app.route('/save-audio-story', methods=['POST'])
-def save_audio_story():
-    global audio_story_loader
-
-    audio_story = flask.request.json
-    audio_story_id = audio_story["story_id"]
-    audio_story_loader.save(audio_story,
-                            audio_story_id,
-                            check_exists=True,
-                            generate_audio=True)
-
-
-@app.route('/load-audio-story/<string:story_id>')
-def load_audio_story(story_id):
-    global audio_story_loader
-
-    graph = audio_story_loader.load(story_id,
-                                    must_have_audio=True,
-                                    audio_relative_to=AUDIO_FILES_DIR)
-
-    return flask.jsonify(graph)
+from server.create_app import (AUDIO_FILES_DIR, AUDIO_STORY_FILES_DIR,
+                               create_app)
 
 
 def run_app(debug=True,
@@ -112,15 +11,7 @@ def run_app(debug=True,
             save_dir=AUDIO_STORY_FILES_DIR,
             audio_save_dir=AUDIO_FILES_DIR):
 
-    global model
-    global audio_story_loader
-
-    model = CLIPModel(device)
-
-    speech_generator = DummySpeechGenerator()
-    audio_story_loader = AudioStoryLoader(save_dir=save_dir,
-                                          audio_save_dir=audio_save_dir,
-                                          speech_generator=speech_generator)
+    app = create_app(device, save_dir, audio_save_dir)
     app.run(debug=debug, port=port, host=host)
 
 
--- server/test/test_api.py
+++ server/test/test_api.py
@@ -1,49 +1,125 @@
+import copy
 import json
 import os
+import shutil
+import unittest
 
-import requests
+from server.create_app import create_app
+from server.speech_generator import DummySpeechGenerator
 
 URL = "http://127.0.0.1:5000/"
 
 
-def _check_request(r):
-    if r.status_code == 200:
-        return
-    raise RuntimeError("Problem http code: {}: {}".format(
-        r.status_code, r.content))
+def _this_dir():
+    return os.path.dirname(os.path.realpath(__file__))
 
 
-def send_image(output_request=True):
-    this_file_dir = os.path.dirname(os.path.realpath(__file__))
-    image_path = os.path.join(this_file_dir, "pug.jpg")
-    endpoint = URL + 'inference'
-    labels = ['dog', 'cat']
-    multipart_form_data = {
-        'image': ('image.jpg', open(image_path, 'rb')),
-        'labels': (None, json.dumps(labels))
-    }
-    r = requests.post(endpoint, files=multipart_form_data)
+class TestApi(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.save_dir = os.path.join(_this_dir(), "tmp")
+        cls.audio_save_dir = os.path.join(_this_dir(), "tmp_audio")
+        cls.client = create_app(
+            device="cuda",
+            save_dir=cls.save_dir,
+            audio_save_dir=cls.audio_save_dir).test_client()
 
-    if output_request:
-        with open("request_body.txt", "wb") as outfile:
-            outfile.write(r.request.body)
+    def setUp(self):
+        if not os.path.exists(self.save_dir):
+            os.makedirs(self.save_dir)
 
-    _check_request(r)
-    print("RESPONSE", r.json())
+        if not os.path.exists(self.audio_save_dir):
+            os.makedirs(self.audio_save_dir)
 
+    def tearDown(self):
+        shutil.rmtree(self.save_dir)
+        shutil.rmtree(self.audio_save_dir)
 
-def send_invalid():
-    this_file_dir = os.path.dirname(os.path.realpath(__file__))
-    image_path = os.path.join(this_file_dir, "pug.jpg")
-    endpoint = URL + 'inference'
-    multipart_form_data = {
-        'image': ('image.jpg', open(image_path, 'rb')),
-        'labels': (None, "blah")
-    }
-    r = requests.post(endpoint, files=multipart_form_data)
-    print(r.content)
+    def _get_dummy_audio_file(self, fname):
+        savepath = os.path.join(self.audio_save_dir, fname)
+        speech_generator = DummySpeechGenerator()
+        speech_generator.to_speech("", savepath=savepath)
+
+    def _get_dummy_audio_graph(self, fname):
+        audio_file_path = os.path.join(_this_dir(), "..", "..", "data",
+                                       "samples", fname)
+        with open(audio_file_path) as infile:
+            graph = json.load(infile)
+
+        return graph
+
+    def test_ok(self):
+        response = self.client.get("/check")
+        self.assertEqual(200, response.status_code)
+
+    def test_inference(self):
+        this_file_dir = os.path.dirname(os.path.realpath(__file__))
+        image_path = os.path.join(this_file_dir, "pug.jpg")
+        labels = ['dog', 'cat']
+        multipart_form_data = {
+            'image': open(image_path, 'rb'),
+            'labels': json.dumps(labels)
+        }
+        response = self.client.post("/inference", data=multipart_form_data)
+        self.client.post()
+        self.assertEqual(200, response.status_code)
+        data = response.get_json()
+        self.assertEqual(2, len(data))
+        self.assertAlmostEqual(sum(data), 1, delta=0.001)
+
+    def test_serve_audio_file(self):
+        fname = "test.mp3"
+        self._get_dummy_audio_file(fname)
+
+        assert os.path.exists(os.path.join(self.audio_save_dir, fname))
+
+        response = self.client.get("/audio-files/{}".format(fname))
+        self.assertEqual(200, response.status_code)
+
+    def test_save_audio_story(self):
+        audio_story_graph = self._get_dummy_audio_graph("one_audio_node.json")
+        audio_story_id = "story_id"
+        audio_story_graph["story_id"] = audio_story_id
+        response = self.client.post("/save-audio-story",
+                                    json=audio_story_graph)
+        self.assertEqual(200, response.status_code)
+
+    def test_load_audio_story(self):
+        audio_story_graph = self._get_dummy_audio_graph("one_audio_node.json")
+        audio_story_id = "story_id"
+        audio_story_graph["story_id"] = audio_story_id
+
+        # post to save the story
+        response = self.client.post("/save-audio-story",
+                                    json=audio_story_graph)
+        self.assertEqual(200, response.status_code)
+
+        # now get the story back
+        response = self.client.get(
+            "/load-audio-story/{}".format(audio_story_id))
+        self.assertEqual(200, response.status_code)
+        returned_graph = response.get_json()
+
+        # Check that I can get the audio files it returns
+        audio_paths = [
+            node["audio_file"] for _, node in returned_graph["nodes"].items()
+        ]
+
+        for audio_path in audio_paths:
+            response = self.client.get("/audio-files/{}".format(audio_path))
+            self.assertEqual(200, response.status_code)
+
+        # check that the two files are equivalent, modulo the audio file key
+        returned_graph_copy = copy.deepcopy(returned_graph)
+
+        for node_id, node in returned_graph_copy["nodes"].items():
+            node.pop("audio_file")
+        self.assertEqual(audio_story_graph, returned_graph_copy)
+
+        # check that the files are what I expected
+        self.assertEqual("{}_0.mp3".format(audio_story_id),
+                         returned_graph['nodes']['0']['audio_file'])
 
 
 if __name__ == "__main__":
-    send_image()
-    # send_invalid()
+    unittest.main()
