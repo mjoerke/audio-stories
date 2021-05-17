fdb1f8d6c23b5092c30548c028d4a741debf85ac
Alan Cheng <alan7cheng@gmail.com>
2021-05-17 01:12:55 -0700

    authoring tool: add link types (WIP)

--- authoring-tool/src/model/CardData.js
+++ authoring-tool/src/model/CardData.js
@@ -4,9 +4,21 @@ import type { UniqueId } from "../util/UniqueId";
 
 export const DEFAULT_CARD_SIZE = 200;
 
+export type SimpleLink = $ReadOnly<{
+  next: UniqueId,
+  type: "simple",
+}>;
+
+export type ClassifierLinks = $ReadOnly<{
+  // link destinations by label
+  labels: { [string]: UniqueId },
+  thresholds: { [string]: number },
+}>;
+
 type BaseCardData = $ReadOnly<{
   id: UniqueId,
   height: number,
+  links: SimpleLink | ClassifierLinks,
   type: "audio_card" | "classifier_card",
   width: number,
   x: number,
@@ -16,12 +28,15 @@ type BaseCardData = $ReadOnly<{
 
 export type AudioCardData = $ReadOnly<{
   ...BaseCardData,
+  links: SimpleLink,
   text: string,
   type: "audio_card",
 }>;
 
 export type ClassifierCardData = $ReadOnly<{
   ...BaseCardData,
+  // TODO: change to ClassifierLinks
+  links: SimpleLink,
   type: "classifier_card",
 }>;
 
