c36333e6f7bfc00df9a6024db87e1129f56e3d23
Alan Cheng <alan7cheng@gmail.com>
2021-05-27 14:42:13 -0700

    authoring tool: draw arrows

---
+++ authoring-tool/src/components/CanvasDrawHandler.js
@@ -0,0 +1,70 @@
+// @flow
+
+import type { CardData } from "../model/CardData";
+import type { UniqueId } from "../util/UniqueId";
+import { SIDE_PANEL_WIDTH } from "../constants/Sizes";
+import getAdjacentCardIds from "../util/CardDataUtils";
+
+const ARROWHEAD_LENGTH = 16;
+
+export function drawLink(
+  ctx: CanvasRenderingContext2D,
+  startCoords: { x: number, y: number },
+  endCoords: { x: number, y: number }
+) {
+  const angle = Math.atan2(
+    endCoords.y - startCoords.y,
+    endCoords.x - startCoords.x
+  );
+  ctx.beginPath();
+  ctx.moveTo(startCoords.x, startCoords.y);
+  ctx.lineTo(endCoords.x, endCoords.y);
+  ctx.lineTo(
+    endCoords.x - ARROWHEAD_LENGTH * Math.cos(angle - Math.PI / 6),
+    endCoords.y - ARROWHEAD_LENGTH * Math.sin(angle - Math.PI / 6)
+  );
+  ctx.moveTo(endCoords.x, endCoords.y);
+  ctx.lineTo(
+    endCoords.x - ARROWHEAD_LENGTH * Math.cos(angle + Math.PI / 6),
+    endCoords.y - ARROWHEAD_LENGTH * Math.sin(angle + Math.PI / 6)
+  );
+  ctx.stroke();
+}
+
+export function drawExistingLinks(
+  ctx: CanvasRenderingContext2D,
+  cards: Map<UniqueId, CardData>
+) {
+  ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+  // eslint-disable-next-line no-undef
+  ctx.canvas.width = window.innerWidth;
+  // eslint-disable-next-line no-undef
+  ctx.canvas.height = window.innerHeight;
+  Array.from(cards).forEach(([from, fromCard]) => {
+    ctx.beginPath();
+    const tos = getAdjacentCardIds(cards, from);
+    tos.forEach((to) => {
+      const toCard = cards.get(to);
+      if (toCard == null) {
+        console.error(
+          // $FlowExpectedError coerce to string for error logging
+          `No card found with id ${toCard.id} when trying to draw link to card!`
+        );
+      } else {
+        /* Card coords are absolute relative to window, so we need to
+         * offset by the size of the side panel */
+        drawLink(
+          ctx,
+          {
+            x: fromCard.x + fromCard.width - SIDE_PANEL_WIDTH,
+            y: fromCard.y + fromCard.height / 2,
+          },
+          {
+            x: toCard.x - SIDE_PANEL_WIDTH,
+            y: toCard.y + toCard.height / 2,
+          }
+        );
+      }
+    });
+  });
+}
